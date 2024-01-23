1dba005174f8de16c25eaa5f80c31d2b5b041b54
Dallin Breen <dallinbreen624@gmail.com>
2024-01-22 23:27:30 -0700

    set up title page with slideshow

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev === 3 ? 1 : prev + 1));
+    }, 7000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="Main">
-      <h1>Home Page</h1>
+      <div className="Title">
+        <div className={`slides slide${currentSlide}`}></div>
+        <div className="title-text">
+          <h1>HAIR BY MADDY KATE</h1>
+          <h3>Blonde | Lived-in Color | Extensions</h3>
+        </div>
+      </div>
+      <div className="review"></div>
+      <div className="review"></div>
     </div>
   );
 }
