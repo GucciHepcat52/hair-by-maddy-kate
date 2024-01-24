690b17a56aba1f267b65f50ed80fcc1b5151ed6f
Dallin Breen <dallinbreen624@gmail.com>
2024-01-23 17:24:08 -0700

    removed unneeded imports

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Components.css";
 import { Button } from "@mui/material";
 import { Instagram } from "@mui/icons-material";
