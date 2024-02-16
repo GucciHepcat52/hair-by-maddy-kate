0cb2e493f499e9c950ad55d1bf2aa414f96e2825
Dallin Breen <dallinbreen624@gmail.com>
2024-02-16 16:17:37 -0700

    finalized initial site

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Components.css";
 import { Button } from "@mui/material";
 import { Instagram } from "@mui/icons-material";
+import { Modal, Box, Fade } from "@mui/material";
 
 import { Link } from "react-router-dom";
 
+const style = {
+  position: "absolute",
+  fontFamily: `"Cormorant", sans-serif`,
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: " 2px solid lightgray",
+  borderRadius: 10,
+  boxShadow: 24,
+  p: 4,
+  backgroundColor: "#f0f0f0",
+  textAlign: "center",
+};
+
 export default function Menu({ isActive, activateMenu }) {
+  const [bookNow, setBookNow] = useState(false);
+
+  function handleModal() {
+    setBookNow((prevVal) => !prevVal);
+  }
+
+  function handleClick() {
+    activateMenu();
+    handleModal();
+  }
   return (
     <div className={`Menu ${isActive ? "active" : ""}`}>
       <Link className="link" to={"/"}>
@@ -17,10 +44,10 @@ export default function Menu({ isActive, activateMenu }) {
       <Link className="link" to={"/services"}>
         <h3>SERVICES</h3>
       </Link>
-      <Link className="link" to={"/gallery"}>
+      {/* <Link className="link" to={"/gallery"}>
         <h3>GALLERY</h3>
-      </Link>
-      <h3 style={{ cursor: "pointer" }} onClick={activateMenu}>
+      </Link> */}
+      <h3 style={{ cursor: "pointer" }} onClick={handleClick}>
         BOOK NOW
       </h3>
       <Button className="socials">
@@ -32,6 +59,26 @@ export default function Menu({ isActive, activateMenu }) {
           <Instagram />
         </a>
       </Button>
+      <Modal open={bookNow} onClose={handleModal}>
+        <Fade in={bookNow}>
+          <Box style={style}>
+            <h3>
+              If you would like to book with me, send me a DM through my
+              Instagram Account!
+            </h3>
+            <Button className="socials">
+              <a
+                href="https://www.instagram.com/hairbymaddykate/"
+                target="blank"
+                style={{ color: "black" }}
+              >
+                {/* <Instagram /> */}
+                CLICK HERE
+              </a>
+            </Button>
+          </Box>
+        </Fade>
+      </Modal>
     </div>
   );
 }
