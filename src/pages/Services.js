46daf26516a80dffa9c6b59add1be92d6b8b05b4
Dallin Breen <dallinbreen624@gmail.com>
2024-02-16 15:26:48 -0700

    updated services page

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -36,6 +36,48 @@ export default function Services() {
       <Menu isActive={activeMenu} />
       <div className="services-text">
         <h1>Services</h1>
+        <div>
+          <ul
+            style={{
+              textAlign: "left",
+            }}
+          >
+            <li>1 Row Extension Initial Install</li>
+            <li>1 Row Extension Move-up</li>
+            <li>2 Row Extension Initial Install</li>
+            <li>2 Row Extension Move-up</li>
+          </ul>
+          <ul
+            style={{
+              textAlign: "left",
+            }}
+          >
+            <li>Full Highlight</li>
+            <li>Partial Highlight</li>
+            <li>Full Balayage</li>
+            <li>Partial Balayage</li>
+          </ul>
+          <ul
+            style={{
+              textAlign: "left",
+            }}
+          >
+            <li>All-over Color (Dark)</li>
+            <li>Root Re-touch (Dark)</li>
+            <li>Trim Added onto Color Service</li>
+            <li>Deep Conditioning Treatment</li>
+          </ul>
+          <ul
+            style={{
+              textAlign: "left",
+            }}
+          >
+            <li>Bridal/Event Updo Style</li>
+            <li>Women's Haircut</li>
+            <li>Money-piece Refresh</li>
+            <li>Toner</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
